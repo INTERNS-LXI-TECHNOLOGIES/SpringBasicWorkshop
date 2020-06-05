6c3546a79b1c66e3288dcf7a5fa6e65a7d2447ac
amritha <amritha.p.m@lxisoft.com>
2020-06-05 17:22:15 +0530

    http://jira.bytatech.com:8080/browse/IN-6230

---
+++ MockExam/MockExam JPA/src/main/java/com/lxisoft/service/ExamService.java
@@ -0,0 +1,18 @@
+package com.lxisoft.service;
+
+import java.util.List;
+
+
+
+import com.lxisoft.entity.Exam;
+
+public interface ExamService {
+
+    public List <Exam> getExams();
+
+    public void saveExam(Exam theExam);
+
+    public Exam getExam(int theId) ;
+
+    public void deleteExam(int theId);
+}
---
+++ MockExam/MockExam JPA/src/main/java/com/lxisoft/service/ExamServiceImpl.java
@@ -0,0 +1,43 @@
+package com.lxisoft.service;
+
+import java.util.List;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lxisoft.entity.Exam;
+import com.lxisoft.repository.ExamRepository;
+
+@Service
+public class ExamServiceImpl  {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    
+    @Transactional
+    public List  <Exam> getExams() {
+        return examRepository.findAll();
+    }
+
+   
+    @Transactional
+    public void saveExam(Exam theExam) {
+        examRepository.save(theExam);
+    }
+
+   
+    @Transactional
+    public Optional<Exam> getExam(int id)  {
+        return examRepository.findById(id);
+    }
+
+    
+    @Transactional
+    public void deleteExam(int theId) {
+        examRepository.deleteById(theId);
+    }
+}
