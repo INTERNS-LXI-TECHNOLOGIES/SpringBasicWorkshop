c493ffd47d21f71b3c9fbc1ae17fa470792f63a4
Ashikafs <ashik.hussain@lxisoft.com>
2020-08-27 17:06:55 +0530

    http://jira.bytatech.com:8080/browse/IN-9863

--- MockExam/src/main/java/com/lxisoft/config/WebConfig.java
+++ MockExam/src/main/java/com/lxisoft/config/WebConfig.java
@@ -17,9 +17,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {
-        "com.lxisoft.*"
-})
+@ComponentScan(basePackages = { "com.lxisoft.*" })
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
