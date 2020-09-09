512b7eb732a9f458eb25ee14790c8aeef6ac2cf7
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-09-09 17:33:05 +0530

    http://jira.bytatech.com:8080/browse/IN-10236

---
+++ OPBookingApp/src/main/java/com/lxisoft/model/Doctor.java
@@ -0,0 +1,23 @@
+package com.lxisoft.model;
+
+public class Doctor {
+
+	private String name, department;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+}
