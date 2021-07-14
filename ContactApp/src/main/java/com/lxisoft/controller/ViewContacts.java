10392f12e24fe8870858a007e6812407f397a144
AbijithP1003 <abijith.p@lxisoft.com>
2021-07-14 17:43:42 +0530

    http://jira.bytatech.com:8080/browse/IN-16770

--- ContactApp/src/main/java/com/lxisoft/controller/ViewContacts.java
+++ ContactApp/src/main/java/com/lxisoft/controller/ViewContacts.java
@@ -1,7 +1,9 @@
 package com.lxisoft.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewContacts {
