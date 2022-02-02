c6282e576a94d0d21918186c4c5ad243f481be76
AbijithP1003 <abijith.p@lxisoft.com>
2022-02-02 15:42:45 +0530

    http://34.87.37.122:8080/browse/IN-20348

--- ContactApp/src/main/java/com/lxisoft/ContactAppApplication.java
+++ ContactApp/src/main/java/com/lxisoft/ContactAppApplication.java
@@ -1,9 +1,11 @@
 package com.lxisoft;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ContactAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContactAppApplication.class,args);
--- ContactApp/src/main/java/com/lxisoft/controller/ContactController.java
+++ ContactApp/src/main/java/com/lxisoft/controller/ContactController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@RestController
+@Controller
 public class ContactController {
    // @Autowired
    // ContactRepository repository;
