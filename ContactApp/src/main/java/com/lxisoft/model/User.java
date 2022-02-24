028c5d59ab380c68c518ee09d206979e64420a4e
AbijithP1003 <abijith.p@lxisoft.com>
2022-02-24 17:50:55 +0530

    http://34.87.37.122:8080/browse/IN-20676

---
+++ ContactApp/src/main/java/com/lxisoft/model/User.java
@@ -0,0 +1,50 @@
+package com.lxisoft.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userId")
+    int userId;
+    @Column(name = "user_name")
+    String userName;
+    @Column(name = "password")
+    String password;
+    @Column(name = "user_role")
+    String userRole;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
