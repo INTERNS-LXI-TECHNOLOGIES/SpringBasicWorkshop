623077083a4c7505c405ba452675eaa8a084a7c7
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2022-04-07 18:33:05 +1000

    Rewrite DAO class with JdbcTemplate

--- UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/config/WebConfig.java
+++ UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/config/WebConfig.java
@@ -1,8 +1,14 @@
 package com.lxisoft.carshowroom.config;
 
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +20,9 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan(basePackages = "com.lxisoft.carshowroom")
 public class WebConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private ServletContext context;
+
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -21,5 +30,23 @@ public class WebConfig implements WebMvcConfigurer {
 		viewResolver.setPrefix("/WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+
+	@Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(context.getInitParameter("jdbcURL"));
+        dataSource.setUsername(context.getInitParameter("jdbcUsername"));
+        dataSource.setPassword(context.getInitParameter("jdbcPassword"));
+        return dataSource;
+    }
+	
+	@Bean
+	@Autowired
+	public JdbcTemplate jdbcTemplate(DataSource mysqlDataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(mysqlDataSource);
+		return jdbcTemplate;
 	}
 }
--- UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/dao/CarDAO.java
+++ UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/dao/CarDAO.java
@@ -1,186 +1,53 @@
 package com.lxisoft.carshowroom.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.ServletContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.lxisoft.carshowroom.model.Car;
 
-@Component
+@Repository
 public class CarDAO {
-	private String jdbcURL;
-	private String jdbcUsername;
-	private String jdbcPassword;
-	private Connection jdbcConnection;
 
 	@Autowired
-	private ServletContext context;
-
-	@PostConstruct
-    private void postConstruct() {
-		this.jdbcURL = context.getInitParameter("jdbcURL");
-		this.jdbcUsername = context.getInitParameter("jdbcUsername");
-		this.jdbcPassword = context.getInitParameter("jdbcPassword");
-	}
-
-	protected void connect() throws SQLException {
-		if (jdbcConnection == null || jdbcConnection.isClosed()) {
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				throw new SQLException(e);
-			}
-			jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-		}
-	}
-
-	protected void disconnect() throws SQLException {
-		if (jdbcConnection != null && !jdbcConnection.isClosed()) {
-			jdbcConnection.close();
-		}
-	}
+	private JdbcTemplate jdbcTemplate;
 
 	public boolean insertCar(Car car) throws SQLException {
 		String sql = "INSERT INTO CAR (manufacturer, model, variant, year, total_kilometers, expected_price, other_details) VALUES (?,?,?,?,?,?,?)";
-		connect();
-
-		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-		statement.setString(1, car.getManufacturer());
-		statement.setString(2, car.getModel());
-		statement.setString(3, car.getVariant());
-		statement.setInt(4, car.getYear());
-		statement.setInt(5, car.getTotalKilometers());
-		statement.setInt(6, car.getExpectedPrice());
-		statement.setString(7, car.getOtherDetails());
-
-		int result = statement.executeUpdate();
+		int result = jdbcTemplate.update(sql, new Object[] { car.getManufacturer(), car.getModel(), car.getVariant(),
+				car.getYear(), car.getTotalKilometers(), car.getExpectedPrice(), car.getOtherDetails() });
 		boolean rowInserted = result > 0;
-		statement.close();
-		disconnect();
 		return rowInserted;
 	}
 
 	public List<Car> listAllCars() throws SQLException {
-		List<Car> carList = new ArrayList<>();
-
 		String sql = "SELECT * FROM car";
-
-		connect();
-
-		Statement statement = jdbcConnection.createStatement();
-		ResultSet resultSet = statement.executeQuery(sql);
-
-		while (resultSet.next()) {
-			int carId = resultSet.getInt("car_id");
-			String manufacturer = resultSet.getString("manufacturer");
-			String model = resultSet.getString("model");
-			String variant = resultSet.getString("variant");
-			int year = resultSet.getInt("year");
-			int totalKilometers = resultSet.getInt("total_kilometers");
-			int expectedPrice = resultSet.getInt("expected_price");
-			String otherDetails = resultSet.getString("other_details");
-
-			Car car = new Car();
-			car.setCarId(carId);
-			car.setManufacturer(manufacturer);
-			car.setModel(model);
-			car.setVariant(variant);
-			car.setYear(year);
-			car.setTotalKilometers(totalKilometers);
-			car.setExpectedPrice(expectedPrice);
-			car.setOtherDetails(otherDetails);
-			carList.add(car);
-		}
-
-		resultSet.close();
-		statement.close();
-
-		disconnect();
-
+		List<Car> carList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<Car>(Car.class));
 		return carList;
 	}
 
 	public boolean deleteCar(Car car) throws SQLException {
 		String sql = "DELETE FROM CAR where car_id = ?";
-
-		connect();
-
-		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-		statement.setInt(1, car.getCarId());
-
-		int result = statement.executeUpdate();
+		int result = jdbcTemplate.update(sql, new Object[] { car.getCarId() });
 		boolean rowDeleted = result > 0;
-		statement.close();
-		disconnect();
 		return rowDeleted;
 	}
 
 	public boolean updateCar(Car car) throws SQLException {
 		String sql = "UPDATE CAR SET manufacturer = ?, model = ?, variant = ?, year = ?, total_kilometers = ?, expected_price = ?, other_details = ? WHERE car_id = ?";
-		connect();
-
-		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-		statement.setString(1, car.getManufacturer());
-		statement.setString(2, car.getModel());
-		statement.setString(3, car.getVariant());
-		statement.setInt(4, car.getYear());
-		statement.setInt(5, car.getTotalKilometers());
-		statement.setInt(6, car.getExpectedPrice());
-		statement.setString(7, car.getOtherDetails());
-		statement.setInt(8, car.getCarId());
-
-		int result = statement.executeUpdate();
+		int result = jdbcTemplate.update(sql, new Object[] { car.getManufacturer(), car.getModel(), car.getVariant(),
+				car.getYear(), car.getTotalKilometers(), car.getExpectedPrice(), car.getOtherDetails(), car.getCarId() });
 		boolean rowUpdated = result > 0;
-		statement.close();
-		disconnect();
 		return rowUpdated;
 	}
 
 	public Car getCar(int carId) throws SQLException {
-		Car car = null;
 		String sql = "SELECT * FROM CAR WHERE car_id = ?";
-
-		connect();
-
-		PreparedStatement statement = jdbcConnection.prepareStatement(sql);
-		statement.setInt(1, carId);
-
-		ResultSet resultSet = statement.executeQuery();
-
-		if (resultSet.next()) {
-			String manufacturer = resultSet.getString("manufacturer");
-			String model = resultSet.getString("model");
-			String variant = resultSet.getString("variant");
-			int year = resultSet.getInt("year");
-			int totalKilometers = resultSet.getInt("total_kilometers");
-			int expectedPrice = resultSet.getInt("expected_price");
-			String otherDetails = resultSet.getString("other_details");
-
-			car = new Car();
-			car.setCarId(carId);
-			car.setManufacturer(manufacturer);
-			car.setModel(model);
-			car.setVariant(variant);
-			car.setYear(year);
-			car.setTotalKilometers(totalKilometers);
-			car.setExpectedPrice(expectedPrice);
-			car.setOtherDetails(otherDetails);
-		}
-
-		resultSet.close();
-		statement.close();
-
+		Car car = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<Car>(Car.class), new Object[] { carId });
 		return car;
 	}
 }
