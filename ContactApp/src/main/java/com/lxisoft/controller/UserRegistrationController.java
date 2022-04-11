892d72f7646e4af6b6731a2e34f863a17e9c25b5
AbijithP1003 <abijith.p@lxisoft.com>
2022-04-11 18:02:44 +0530

    Bug fixing

--- ContactApp/src/main/java/com/lxisoft/controller/UserRegistrationController.java
+++ ContactApp/src/main/java/com/lxisoft/controller/UserRegistrationController.java
@@ -29,7 +29,7 @@ public class UserRegistrationController {
     @GetMapping("/registration")
     public String registration(Model model) {
         User user = new User();
-        model.addAttribute("userForm", user);
+        model.addAttribute("user", user);
 
         return "registration";
     }
