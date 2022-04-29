00e51d23e13e9e683d29ce26e231c15adee356d3
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2022-04-29 19:03:46 +1000

    Hibernate configurations

--- UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/config/WebConfig.java
+++ UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/config/WebConfig.java
@@ -1,14 +1,19 @@
 package com.lxisoft.carshowroom.config;
 
-import javax.servlet.ServletContext;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -16,11 +21,13 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@PropertySource("classpath:database.properties")
 @ComponentScan(basePackages = "com.lxisoft.carshowroom")
 public class WebConfig {
 
 	@Autowired
-	private ServletContext context;
+	private Environment environment;
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -32,20 +39,37 @@ public class WebConfig {
 	}
 
 	@Bean
-    public DataSource mysqlDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(context.getInitParameter("jdbcURL"));
-        dataSource.setUsername(context.getInitParameter("jdbcUsername"));
-        dataSource.setPassword(context.getInitParameter("jdbcPassword"));
-        return dataSource;
-    }
-	
+	public DataSource mysqlDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
+	}
+
 	@Bean
-	@Autowired
-	public JdbcTemplate jdbcTemplate(DataSource mysqlDataSource) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		jdbcTemplate.setDataSource(mysqlDataSource);
-		return jdbcTemplate;
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(mysqlDataSource());
+		sessionFactory.setPackagesToScan(new String[] { "com.lxisoft.carshowroom.model" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		return properties;
+	}
+
+	@Bean
+	public HibernateTransactionManager getTransactionManager() {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory().getObject());
+		return transactionManager;
 	}
 }
