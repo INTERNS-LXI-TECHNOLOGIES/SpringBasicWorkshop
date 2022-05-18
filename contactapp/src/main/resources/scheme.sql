4df9860157bcc32b0f3a66b76220a6b1f49143d8
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2022-05-18 13:07:47 +0530

    SecurityImplimented

--- contactapp/src/main/resources/scheme.sql
+++ contactapp/src/main/resources/scheme.sql
@@ -10,3 +10,17 @@ CREATE TABLE `contact` (
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 
+CREATE TABLE `user` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(15) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `role` (`role`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `role` (`role_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `role` (
+  `role_name` varchar(15) NOT NULL,
+  PRIMARY KEY (`role_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
