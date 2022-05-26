8b2b86e09e4272fdfbe9acb1f98f3c37d0306a81
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2022-05-26 08:42:02 +0530

    JpA started

--- contactapp/src/main/java/com/lxisoft/spring/config/SpringMvcConfig.java
+++ contactapp/src/main/java/com/lxisoft/spring/config/SpringMvcConfig.java
@@ -48,14 +48,7 @@ public class SpringMvcConfig  {
         resolver.setSuffix(".jsp");
         return resolver;
     }
-/*    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(mysqlDataSource());
-        sessionFactory.setPackagesToScan(new String[] { "com.lxisoft.spring.entity" });
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }*/
+
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
